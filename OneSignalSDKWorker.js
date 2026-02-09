407220cbfef3e146fa29c020ecbc040abf9a624a
Josias-Projet <josiasakodjenou736@gmail.com>
2026-02-09 11:16:38 +0100

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,5 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+// Ajout optionnel pour stabiliser
+self.addEventListener('message', (event) => {
+  // Pass-through
+});
